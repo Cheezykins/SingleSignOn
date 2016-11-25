aa68c25f6cfa3536841ff5b4b6f3dca07d799a1a
chris.stretton <chris.stretton@fasthosts.com>
2016-11-25 15:03:12 +0000

    Added docblock messages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers;
 use App\Disk;
 use Illuminate\Http\Request;
 
+/**
+ * Controls the homepage
+ * Class HomeController
+ * @package App\Http\Controllers
+ */
 class HomeController extends Controller
 {
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $diskSpace = Disk::all();
         return view('home', ['disks' => $diskSpace]);
     }
 
+    /**
+     * Route intercepted by middleware.
+     */
     public function check()
     {
         abort(403, "You are not welcome");
