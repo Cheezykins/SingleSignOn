4d054cd564099aebe4b35afc9948accef56c6b5f
Chris Stretton <cstretton@gmail.com>
2016-11-29 22:17:27 +0000

    Updated views to show links from the DB

--- database/migrations/2016_11_27_231943_define_base_links.php
+++ database/migrations/2016_11_27_231943_define_base_links.php
@@ -21,55 +21,73 @@ class DefineBaseLinks extends Migration
         $requests->name = 'Requests';
         $requests->save();
 
+        $toolingRole = new \App\Role();
+        $toolingRole->name = 'Can view tooling links';
+        $toolingRole->code = 'VIEW_TOOLS';
+        $toolingRole->save();
+
+        $requestsRole = new \App\Role();
+        $requestsRole->name = 'Can view requests links';
+        $requestsRole->code = 'VIEW_REQUESTS';
+        $requestsRole->save();
+
+
         $links = [
             [
                 'name' => 'SabNZBD',
                 'url' => 'https://dorgin.cheez.systems/sabnzbd',
                 'icon_name' => 'sabnzbd.png',
                 'description' => 'Usenet download client (actually does the downloading)',
-                'category' => $tooling
+                'category' => $tooling,
+                'role' => $toolingRole
             ],
             [
                 'name' => 'Couch Potato',
                 'url' => 'https://dorgin.cheez.systems/couchpotato',
                 'icon_name' => 'couch.png',
                 'description' => 'Manage and select movies to download',
-                'category' => $tooling
+                'category' => $tooling,
+                'role' => $toolingRole
             ],
             [
                 'name' => 'Sonarr',
                 'url' => 'https://dorgin.cheez.systems/sonarr',
                 'icon_name' => 'sonarr.png',
                 'description' => 'Manage and select TV shows to download',
-                'category' => $tooling
+                'category' => $tooling,
+                'role' => $toolingRole
             ],
             [
                 'name' => 'Deluge',
                 'url' => 'https://talyn.cheez.systems:8112',
                 'icon_name' => 'deluge.png',
                 'description' => 'Torrent download client (does downloading on the rare occasion we use torrents)',
-                'category' => $tooling
+                'category' => $tooling,
+                'role' => $toolingRole
             ],
             [
                 'name' => 'Request new TV and Movies',
                 'url' => 'https://plex.cheez.systems/requests',
                 'icon_name' => 'requests.png',
                 'description' => 'Request new TV shows and Movies be added to plex',
-                'category' => $requests
+                'category' => $requests,
+                'role' => $requestsRole
             ],
             [
                 'name' => 'Dorgin Statistics',
                 'url' => 'https://plex.cheez.systems/stats/dorgin',
                 'icon_name' => 'stats.png',
                 'description' => 'Statistics for the Dorgin Plex server',
-                'category' => $requests
+                'category' => $requests,
+                'role' => $requestsRole
             ],
             [
                 'name' => 'Talyn Statistics',
                 'url' => 'https://plex.cheez.systems/stats/talyn',
                 'icon_name' => 'stats.png',
                 'description' => 'Statistics for the Talyn Plex server',
-                'category' => $requests
+                'category' => $requests,
+                'role' => $requestsRole
             ]
         ];
 
@@ -82,6 +100,7 @@ class DefineBaseLinks extends Migration
             $link->description = $linkDetail['description'];
             $link->category()->associate($linkDetail['category']);
             $link->save();
+            $link->roles()->attach($linkDetail['role']);
         }
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,31 +4,22 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            @if(Auth::user()->canAccess('dorgin.cheez.systems'))
+            @foreach(Auth::user()->linksByCategory() as $category => $links)
                 <div class="panel panel-default">
-                    <div class="panel-heading">Tooling Links</div>
+                    <div class="panel-heading">{{ $category }}</div>
                     <div class="panel-body">
                         <ul class="linkList">
-                            <li data-toggle="tooltip" data-placement="top" title="Usenet download client (actually does the downloading)"><a href="https://dorgin.cheez.systems/sabnzbd"><img src="{{ asset('images/sabnzbd.png') }}" />SabNZBD</a></li>
-                            <li data-toggle="tooltip" data-placement="top" title="Manage and select movies to download"><a href="https://dorgin.cheez.systems/couchpotato"><img src="{{ asset('images/couch.png') }}" />Couch Potato</a></li>
-                            <li data-toggle="tooltip" data-placement="top" title="Manage and select TV shows to download"><a href="https://dorgin.cheez.systems/sonarr"><img src="{{ asset('images/sonarr.png') }}" />Sonarr</a></li>
-                            <li data-toggle="tooltip" data-placement="top" title="Torrent download client (does downloading on the rare occasion we use torrents)"><a href="https://talyn.cheez.systems:8112"><img src="{{ asset('images/deluge.png') }}"/>Deluge</a></li>
+                            @foreach ($links as $link)
+                                <li data-toggle="tooltip" data-placement="top" title="{{ $link->description }}">
+                                    <a href="{{ $link->url }}">
+                                        <img src="{{ asset('images/' . $link->icon_name) }}" /> {{ $link->name }}
+                                    </a>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
-            @endif
-            @if(Auth::user()->canAccess('plex.cheez.systems'))
-                <div class="panel panel-default">
-                    <div class="panel-heading">Requests</div>
-                    <div class="panel-body">
-                        <ul class="linkList">
-                            <li data-toggle="tooltip" data-placement="top" title="Request new TV shows and Movies be added to plex"><a href="https://plex.cheez.systems/requests"><img src="{{ asset('images/requests.png') }}"/>Request new TV and Movies</a></li>
-                            <li data-toggle="tooltip" data-placement="top" title="Statistics for the Dorgin Plex server"><a href="https://plex.cheez.systems/stats/dorgin"><img src="{{ asset('images/stats.png') }}"/>Dorgin Statistics</a></li>
-                            <li data-toggle="tooltip" data-placement="top" title="Statistics for the Talyn Plex server"><a href="https://plex.cheez.systems/stats/talyn"><img src="{{ asset('images/stats.png') }}"/>Talyn Statistics</a></li>
-                        </ul>
-                    </div>
-                </div>
-            @endif
+            @endforeach
         </div>
     </div>
 </div>
